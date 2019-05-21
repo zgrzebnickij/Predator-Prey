5245e942e9c1437a3dcc9b62985479679770f18f
Jakub Zgrzebnicki <jakub.zgrzeb@gmail.com>
2019-05-21 17:50:12 +0200

    Fix building preject issue

--- Predator-Prey/main.cpp
+++ Predator-Prey/main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
 	std::cout << "Hello world" << std::endl;
-	auto prey = Prey();
+	//auto prey = Prey();
 	Logger::getInstance().Log("test", "Przyklad loggera");
 	Logger::getInstance().Log("test", "Tak go uzywac");
 	Logger::getInstance().Log("inny_log", "Pliki sa tworzone i dopisywane na koncu");
